a463a7d8a44f148260a7d75a03be2dece2ff4956
Grokit Team <usegrokit@gmail.com>
2026-09-12 00:16:00 +0530

    Configure Amplify frontend authentication

--- main.tsx
+++ main.tsx
@@ -3,15 +3,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-// import { Amplify } from 'aws-amplify';
-// import outputs from './amplify_outputs.json';
+import { Amplify } from 'aws-amplify';
+import outputs from './amplify_outputs.json';
 import { AppProviders } from './pages/providers';
 import App from './pages/App';
 import './pages/index.css';
 
-// Amplify.configure(outputs);
+Amplify.configure(outputs);
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
   throw new Error('Root element with id "root" was not found in index.html');
 }
